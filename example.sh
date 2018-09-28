ddb018c796a0a78fe3880478809a43b7a63197ee
Ore Poran <ore@soluto.com>
2018-09-28 16:10:11 +0200

    finish example

--- example.sh
+++ example.sh
@@ -2,6 +2,8 @@
 
 # Which branch are you comparing to? In most cases it will be master
 DEFAULT_BRANCH='master'
+TOKEN_WEB='web'
+TOKEN_MOBILE='mobile'
 
 # In Teamcity, this is how you get the current commit that triggered the CI
 CURRENT_HASH=%system.build.vcs.number% 
@@ -10,4 +12,20 @@ CURRENT_HASH=%system.build.vcs.number%
 CHANGED_FILES=$(git --no-pager diff --name-only $CURRENT_HASH $(git merge-base $CURRENT_HASH $DEFAULT_BRANCH))
 
 # Run git-diffy
-git-diffy run --diffys web mobile --files=$(echo $CHANGED_FILES)
+DIFFYS=$(git-diffy run --diffys $TOKEN_WEB $TOKEN_MOBILE --files=$(echo $CHANGED_FILES))
+
+# CI Logic
+
+if [[ $DIFFYS = *$TOKEN_WEB* ]]; then
+  echo "Run web!!"
+fi
+
+if [[ $DIFFYS = *$TOKEN_MOBILE* ]]; then
+  echo "Run mobile!!"
+fi
+
+if [[ $DIFFYS = *$"other"* ]]; then
+  echo "Run web && mobile!!"
+fi
+
+
